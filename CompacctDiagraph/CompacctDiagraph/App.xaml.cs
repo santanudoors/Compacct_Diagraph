7fc24bb2a356e8d661336c260d31eb3a32b21c99
Santanu <santan@doorsonline.in>
2017-10-23 17:25:45 +0530

    23-10-2017-2

--- CompacctDiagraph/CompacctDiagraph/App.xaml.cs
+++ CompacctDiagraph/CompacctDiagraph/App.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace CompacctDiagraph
 {
@@ -12,6 +15,10 @@ namespace CompacctDiagraph
         public App()
         {
             InitializeComponent();
+
+            MobileCenter.Start("android=a25ce643-f86a-4c74-b1bd-e68a39195d6f;" + "uwp={Your UWP App secret here};" +
+                   "ios={Your iOS App secret here}",
+                   typeof(Analytics), typeof(Crashes));
 
             MainPage = new CompacctDiagraph.MainPage();
         }
